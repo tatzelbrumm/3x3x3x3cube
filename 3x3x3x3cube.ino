e85305fc26b1630282db1223e50f91409e22acf7
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2023-10-29 16:41:17 +0100

    3x3x3x3cube.ino : count to 81, 500ms delay, 2000ms end delay, cycle colors

--- 3x3x3x3cube.ino
+++ 3x3x3x3cube.ino
@@ -3,37 +3,35 @@
 int leds= 27;
 // create a pixel strand with 2 pixels on pin A3, color sequence RGB
 Adafruit_NeoPixel pixels(leds, 3, NEO_RGB);
-unsigned long colors[8];
+unsigned long colors[3];
 byte offset;
 
 void setup() 
 {
-  byte brightness= 0x20;
-  pixels.begin();  // initialize the pixels
-  for(int c= 0; c<8; c++)
+  unsigned long brightness= 0x20;
+  for (int c=0; c<3; c++)
   {
-    int g;
-    g= c ^ (c>>1);
-    unsigned long color= 0;
-    for(int rgb= 0; rgb<3; rgb++)
-    {
-      color |= (((unsigned long)1 & (g >> rgb)) * brightness) << ((2-rgb)<<3); 
-    }
-    colors[c]= color;
+    colors[c]= brightness << (8 * (2-c));
   }
+  pixels.begin();  // initialize the pixels
   offset= 0;
 }
 
 void loop() 
 {
-  for (int p= 0; p<leds; p++)
-  {
-    pixels.setPixelColor(p, colors[(p+offset)%8]);
-  }
-  // and write the data
+  unsigned long color= 0;
+  pixels.clear();
   pixels.show();
-
-  delay(1000);
-  
-  offset= ++offset % 8;
+  for (int c=0; c<3; c++)
+  {
+    color |= colors[(c+offset)%3];
+    for (int p=0; p<leds; p++)
+    {
+      pixels.setPixelColor(p, color);
+      delay(500);
+      pixels.show();
+    }
+  }
+  delay(2000);
+  offset= ++offset % 3;
 }
