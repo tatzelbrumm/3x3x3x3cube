727594770891fadfea7f6463d45db5c3643b4aca
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2023-11-29 00:31:33 +0100

    3x3x3x3cube.ino : brightness 0x10, snaking sequence

--- 3x3x3x3cube.ino
+++ 3x3x3x3cube.ino
@@ -1,10 +1,17 @@
 #include <Adafruit_NeoPixel.h>
 
-int leds= 27;
+const int leds= 27;
 // create a pixel strand with 2 pixels on pin A3, color sequence RGB
 Adafruit_NeoPixel pixels(leds, 3, NEO_RGB);
 unsigned long colors[3];
-byte permutations[6][3]=
+byte sequence[leds]=
+{
+  13,  4,  3,  0,  1,  2,  5,  8,  7, 
+   6, 15, 12,  9, 10, 11, 14, 17, 16, 
+  21, 18, 19, 20, 23, 26, 25, 24, 22
+};
+
+const byte permutations[6][3]=
 {
   {0,1,2},{1,2,0},{2,0,1},
   {0,2,1},{1,0,2},{2,1,0}
@@ -13,7 +20,7 @@ byte perm;
 
 void setup() 
 {
-  unsigned long brightness= 0x20;
+  unsigned long brightness= 0x10;
   for (int c=0; c<3; c++)
   {
     colors[c]= brightness << (8 * (2-c));
@@ -32,7 +39,7 @@ void loop()
     color |= colors[permutations[perm][c]];
     for (int p=0; p<leds; p++)
     {
-      pixels.setPixelColor(p, color);
+      pixels.setPixelColor(sequence[p], color);
       delay(500);
       pixels.show();
     }
