81b2b87d210f14821e4d2ca200ac840af37ba388
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2023-12-01 04:49:52 +0100

    3x3x3x3cube.ino : x,y,z address, count z first, use pin 3, GRB

--- 3x3x3x3cube.ino
+++ 3x3x3x3cube.ino
@@ -1,8 +1,10 @@
 #include <Adafruit_NeoPixel.h>
 
-int leds= 27;
-// create a pixel strand with 2 pixels on pin 0, color sequence GRB
-Adafruit_NeoPixel pixels(leds, 0, NEO_GRB);
+#define ADR(x,y,z) ((x)+(y*3)+(z*9))
+
+const int leds= 27;
+// create a pixel strand with 2 pixels on pin 3, color sequence GRB
+Adafruit_NeoPixel pixels(leds, 3, NEO_GRB);
 unsigned long colors[3];
 byte permutations[6][3]=
 {
@@ -30,12 +32,14 @@ void loop()
   for (int c=0; c<3; c++)
   {
     color |= colors[permutations[perm][c]];
-    for (int p=0; p<leds; p++)
-    {
-      pixels.setPixelColor(p, color);
-      delay(500);
-      pixels.show();
-    }
+    for (int x=0; x<3; x++)
+      for (int y=0; y<3; y++)
+        for (int z=0; z<3; z++)
+        {
+          pixels.setPixelColor(ADR(x,y,z), color);
+          delay(500);
+          pixels.show();
+        }
   }
   delay(2000);
   perm= ++perm % 6;
