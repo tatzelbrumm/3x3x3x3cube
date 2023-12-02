bfa601912ae0dbc3dfabb7ee120dde77ff528259
Christoph Maier <cm.hardware.software.elsewhere@gmail.com>
2023-12-03 00:43:30 +0100

    3x3x3x3cube.ino : add swedish flag

--- 3x3x3x3cube.ino
+++ 3x3x3x3cube.ino
@@ -9,10 +9,12 @@ const byte cube[leds]=
 };
 #define ADR(x,y,z) cube[(x)+(y)*3+(z)*9]
 
+const unsigned long brightness= 0x10;
+
 // create a pixel strand with 27 pixels on pin 3, color sequence GRB
 Adafruit_NeoPixel pixels(leds, 3, NEO_GRB);
 unsigned long colors[3];
-byte sequence[leds]=
+const byte sequence[leds]=
 {
   ADR(1,1,1), 
   ADR(1,1,0), ADR(1,1,2), 
@@ -25,6 +27,18 @@ byte sequence[leds]=
   ADR(2,0,2), ADR(2,2,2), ADR(0,2,2), ADR(0,0,2)
 };
 
+const byte cross[]=
+{
+  ADR(1,1,1), 
+  ADR(1,1,0), ADR(1,1,2), 
+  ADR(0,1,1), ADR(2,1,1), 
+  ADR(1,0,1), ADR(1,2,1),  
+  ADR(1,0,0), ADR(1,0,2), ADR(1,2,2), ADR(1,2,0),
+  ADR(0,1,0), ADR(0,1,2), ADR(2,1,2), ADR(2,1,0),
+  ADR(0,0,1), ADR(0,2,1), ADR(2,2,1), ADR(2,0,1)
+};
+const int nx= 19;
+
 const byte permutations[6][3]=
 {
   {0,1,2},{1,2,0},{2,0,1},
@@ -32,18 +46,7 @@ const byte permutations[6][3]=
 };
 byte perm;
 
-void setup() 
-{
-  unsigned long brightness= 0x80;
-  for (int c=0; c<3; c++)
-  {
-    colors[c]= brightness << (8 * (2-c));
-  }
-  pixels.begin();  // initialize the pixels
-  perm= 0;
-}
-
-void loop() 
+void countup()
 {
   unsigned long color= 0;
   pixels.clear();
@@ -58,6 +61,43 @@ void loop()
       pixels.show();
     }
   }
-  delay(2000);
   perm= ++perm % 6;
+}
+
+void sweden()
+{
+  const unsigned long blue= (brightness*107/256)<<8 | (brightness*167/256);
+  const unsigned long yellow= (brightness*254/256)<<16 | (brightness*204/256)<<8 | (brightness*2/256);
+  for (int p=0; p<leds; p++)
+  {
+    pixels.setPixelColor(sequence[p], blue);
+    delay(500);
+    pixels.show();
+  }
+  for (int p=0; p<nx; p++)
+  {
+    pixels.setPixelColor(cross[p], yellow);
+    delay(500);
+    pixels.show();
+  }
+}
+
+void setup() 
+{
+  for (int c=0; c<3; c++)
+  {
+    colors[c]= brightness << (8 * (2-c));
+  }
+  pixels.begin();  // initialize the pixels
+  perm= 0;
+}
+
+void loop() 
+{
+  countup();
+  delay(1000);
+//pixels.clear();
+//pixels.show();
+  sweden();
+  delay(2000);
 }
